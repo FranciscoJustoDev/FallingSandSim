203f22be9deffe8b3f2e700a63338a5c73073efe
Francisco Justo <franciscojusto.dev@gmail.com>
2023-02-15 23:29:53 +0000

    Initial Commit

---
+++ main.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SAVE 0
+#define CHECK 1
+#define WALL '#'
+#define SAND 'o'
+#define WATER '~'
+#define OPEN_SPACE '.'
+#define LEVEL_TEST "test.txt"
+#define LEVEL_1 "heart.txt"
+#define LEVEL_2 "hi.txt"
+
+typedef struct {
+  int y;
+  int x;
+  int count;
+} Spawner;
+
+typedef struct {
+  char **map;
+  char **second_map;
+  int cols;
+  int rows;
+} Map;
+
+Map *createMap(char *level) {
+  Map *newMap = malloc(sizeof(Map));
+
+  FILE *fp = fopen(level, "r");
+  if (fp == NULL) {
+    printf("Error: File not found\n");
+    exit(1);
+  }
+  int ch;
+  int i = 0;
+  int j = 0;
+  int rows_read = 0;
+
+  while ((ch = fgetc(fp)) != EOF) {
+    j++;
+    if (ch == '\n') {
+      if (rows_read == 0) {
+        rows_read = 1;
+        newMap->rows = j - 1;
+      }
+      i++;
+      j = 0;
+    }
+  }
+  newMap->cols = i + 1;
+
+  newMap->map = malloc(sizeof(char *) * newMap->cols);
+  for (int i = 0; i < newMap->cols; i++) {
+    newMap->map[i] = malloc(sizeof(char) * newMap->rows);
+  }
+  newMap->second_map = malloc(sizeof(char *) * newMap->cols);
+  for (int i = 0; i < newMap->cols; i++) {
+    newMap->second_map[i] = malloc(sizeof(char) * newMap->rows);
+  }
+
+  fseek(fp, 0, SEEK_SET);
+  i = 0;
+  j = 0;
+  while ((ch = fgetc(fp)) != EOF) {
+    newMap->map[i][j] = ch;
+    j++;
+    if (ch == '\n') {
+      j = 0;
+      i++;
+    }
+  }
+
+  free(fp);
+  return newMap;
+}
+
+int drawMap(Map *map) {
+  for (int i = 0; i < map->cols; i++) {
+    for (int j = 0; j < map->rows; j++) {
+      printf("%c", map->map[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
+int spawn(Map *map, Spawner *spawner, char element) {
+  map->map[spawner->y][spawner->x] = element;
+  spawner->count++;
+  return 0;
+}
+
+int check(int op, Map *map, Spawner *spawner) {
+  // save the map sand positions
+  // check against old positions to verify movement
+  int is_diff = 0;
+  switch (op) {
+  case SAVE:
+    for (int i = 0; i < map->cols; i++) {
+      for (int j = 0; j < map->rows; j++) {
+        map->second_map[i][j] = map->map[i][j];
+      }
+    }
+    break;
+  case CHECK:
+    for (int i = 0; i < map->cols; i++) {
+      for (int j = 0; j < map->rows; j++) {
+        if (map->map[i][j] != map->second_map[i][j]) {
+          is_diff = 1;
+        }
+      }
+    }
+    break;
+  default:
+    break;
+  }
+  return is_diff;
+}
+int logic(Map *map, Spawner *spawner, char element, char *dir) {
+  // save
+  check(SAVE, map, spawner);
+  // move sand
+  for (int i = map->cols - 1; i >= 0; i--) {
+    for (int j = 0; j < map->rows; j++) {
+      if (map->map[i][j] == SAND) {
+        if (map->map[i + 1][j] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j] = SAND;
+        } else if (map->map[i + 1][j - 1] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j - 1] = SAND;
+        } else if (map->map[i + 1][j + 1] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j + 1] = SAND;
+        }
+      } else if (map->map[i][j] == WATER) {
+        // down
+        if (map->map[i + 1][j] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j] = WATER;
+        } // bottom left
+        else if (map->map[i + 1][j - 1] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j - 1] = WATER;
+        } // bottom right
+        else if (map->map[i + 1][j + 1] == OPEN_SPACE) {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i + 1][j + 1] = WATER;
+        } else if (map->map[i][j - 1] == OPEN_SPACE && *dir == 'l') {
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i][j - 1] = WATER;
+        } else if (map->map[i][j + 1] == OPEN_SPACE) {
+          *dir = 'r';
+          map->map[i][j] = OPEN_SPACE;
+          map->map[i][j + 1] = WATER;
+        }
+      }
+    }
+  }
+
+  if (map->map[spawner->y][spawner->x] == element) {
+    return 1;
+  }
+  // check and spawn
+  if (check(CHECK, map, spawner) != 1) {
+    *dir = 'l';
+    spawn(map, spawner, element);
+  }
+  return 0;
+}
+
+int main() {
+  Map *map = createMap(LEVEL_1);
+
+  Spawner spawner = {0, 4, 0};
+  int next_spawn = 0;
+  char dir = 'l';
+
+  // initial present level with delay before starting sim
+  system("clear");
+  drawMap(map);
+  sleep(2);
+  system("clear");
+
+  int run = 1;
+  while (run) {
+    drawMap(map);
+    usleep(125000);
+    if (next_spawn == 0) {
+      if (logic(map, &spawner, SAND, &dir) == 1) {
+        spawner.x += 8;
+        next_spawn = 1;
+      }
+    } else if (next_spawn == 1) {
+      if (logic(map, &spawner, WATER, &dir) == 1) {
+        run = 0;
+      }
+    }
+    system("clear");
+  }
+  printf("Particles spawned: %d.\n", spawner.count);
+  return 0;
+}
